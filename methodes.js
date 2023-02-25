beb38ae5afaf4d890550624c74ebbee68aeb30a3
LamineOusmane <126349976+LamineOusmane@users.noreply.github.com>
2023-02-25 20:04:59 +0100

    Add files via upload

---
+++ methodes.js
@@ -0,0 +1,17 @@
+var personne={
+nom:"Mohamed",
+prenom:"Ousmane Ibrahim",
+Age: "21ans",
+NomComplet(){
+
+    return this.prenom+" "+this.nom;
+}
+ 
+};
+
+console.log(personne.propertyMohamed);
+ console.log(personne.NomComplet());
+ 
+ let a=2, b=3, result=4;
+ result -= a + b;
+console.log(result);
